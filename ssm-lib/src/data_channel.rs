f9065032006fdb37843eb267cac3f775d22e3c3d
Brian Heise <bnheise@gmail.com>
2025-03-29 21:18:41 +0900

    add data channel scaffold with mocks

--- ssm-lib/src/data_channel.rs
+++ ssm-lib/src/data_channel.rs
@@ -1,1 +1,11 @@
 //! Implements a data channel for interactive session.
+
+/// TODO: Add a description of the data channel.
+#[mockall::automock]
+pub trait DataChannel {}
+
+/// TODO: Add a description of the default data channel.
+#[derive(Debug, Default)]
+pub struct DefaultDataChannel {}
+
+impl DataChannel for DefaultDataChannel {}
