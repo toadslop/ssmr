95ac433250832987b9125362af077a9064e3e52f
Brian Heise <bnheise@gmail.com>
2025-04-01 20:52:45 +0900

    adds websocket_channel initialization

--- ssm-lib/src/websocket_channel.rs
+++ ssm-lib/src/websocket_channel.rs
@@ -2,18 +2,38 @@
 
 /// TODO: Add a description of the data channel.
 #[mockall::automock]
-pub trait WebsocketChannel {}
+pub trait WebsocketChannel {
+    /// TODO: document
+    fn get_stream_url(&self) -> &str;
+
+    /// TODO: document
+    fn get_channel_token(&self) -> &str;
+}
 
 /// Default [`WebsocketChannel`] implementation.
 #[derive(Debug, Default)]
-pub struct DefaultWebsocketChannel {}
+pub struct DefaultWebsocketChannel {
+    stream_url: String,
+    channel_token: String,
+}
 
-impl WebsocketChannel for DefaultWebsocketChannel {}
+impl WebsocketChannel for DefaultWebsocketChannel {
+    fn get_stream_url(&self) -> &str {
+        &self.stream_url
+    }
+
+    fn get_channel_token(&self) -> &str {
+        &self.channel_token
+    }
+}
 
 impl DefaultWebsocketChannel {
     /// Initialize with default settings.
     #[must_use]
-    pub fn new() -> Self {
-        DefaultWebsocketChannel::default()
+    pub fn new(stream_url: String, channel_token: String) -> Self {
+        Self {
+            stream_url,
+            channel_token,
+        }
     }
 }
